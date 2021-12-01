9474f18cbe04bae85facd80cfeac7bc201dbca89
v-lavs <lavskavanda@gmail.com>
2021-12-02 00:06:30 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+//= include ../../node_modules/waypoints/lib/jquery.waypoints.min.js ;
+//= include ../../node_modules/jquery-nice-select/js/jquery.nice-select.js
+//= include ../../node_modules/baguettebox.js/dist/baguetteBox.min.js ;
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    const nav = $('.header__nav');
+
+    $('.btn_burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn_close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+    // SMOOTH SCROLL TO ANCHOR
+    function smoothScrollToAnchor(selector) {
+        $(selector).on('click', function (event) {
+            let anchor = $.attr(this, 'href')
+
+            if (anchor.match(/^#/) && anchor !== '#') {
+                event.preventDefault()
+                let offsetSize = $("header").innerHeight();
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top - offsetSize
+                }, 2000);
+                nav.removeClass('open');
+                jQuery('.backdrop').fadeOut();
+                $('body').removeClass('modal_open');
+            }
+        })
+
+    }
+
+    smoothScrollToAnchor('.btn_scroll');
+    smoothScrollToAnchor('.menu__link');
+
+
+    //    ANIMATION
+
+    // setTimeout(function () {
+    //     $('.section-banner').addClass('anim_started');
+    // }, 500);
+});
+
+
