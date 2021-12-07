ef3c070fe8c579ae43f16d53907234d9c363f3f8
v-lavs <lavskavanda@gmail.com>
2021-12-07 17:24:40 +0200

    popup  svg

--- src/js/main.js
+++ src/js/main.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
     //HEADER RIGHT BAR
     $('#openBar').click(function (e) {
         $('.right-bar').toggleClass('open-bar');
+        $(".right-bar__content").fadeToggle();
     });
 
     // MODAL
