8685c90c15a3b55ef5099fa06a36d0ca574bfeea
v-lavs <lavskavanda@gmail.com>
2021-12-10 12:07:09 +0200

    add popover

--- src/js/main.js
+++ src/js/main.js
@@ -45,30 +45,6 @@ $(document).ready(function () {
         $popover.html('');
     });
 
-    // Parallax
-
-    // var controller = new ScrollMagic.Controller({
-    //     globalSceneOptions: {
-    //         triggerHook: 'onLeave',
-    //         duration: "200%" // this works just fine with duration 0 as well
-    //         // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-    //         // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-    //     }
-    // });
-    //
-    // // get all slides
-    // var slides = document.querySelectorAll(".section");
-    //
-    // // create scene for every slide
-    // for (var i=0; i<slides.length; i++) {
-    //     new ScrollMagic.Scene({
-    //         triggerElement: slides[i]
-    //     })
-    //         .setPin(slides[i], {pushFollowers: false})
-    //         .addTo(controller);
-    // }
-
-
     //MOBILE MENU
     const nav = $('.header__nav');
 
@@ -199,6 +175,8 @@ $(document).ready(function () {
         })
     });
 
+    //TABS
+
     $(".scheme-tab").each(function() {
         var $this = $(this),
             child = $this.children(":first");
@@ -206,12 +184,16 @@ $(document).ready(function () {
             return child[0].getBoundingClientRect().height;
         });
     });
-
+    const tabLink = $('.scheme-tabs__link');
+    const tabContentItem = $('.scheme-tab');
+    tabLink.click(function (e) {
+        e.preventDefault();
+        tabLink.removeClass('active');
+        tabContentItem.removeClass('active');
+        $(e.target).addClass('active');
+        $($(e.currentTarget).attr('href')).addClass('active');
+    });
 });
 
-// jQuery(window).scroll(function() {
-//     var scrolledY = jQuery(window).scrollTop();
-    // jQuery('.facade-residence_parallax').css('background-position', 'center ' + ((scrolledY)) + 'px');
-// });
 
 
